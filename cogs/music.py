cbbf4238d6c68fb574e5cc975bdea96a2d92ed12
imri0t <37715614+imri0t@users.noreply.github.com>
2020-05-13 11:09:37 -0700

    playf command adjusted
    webm file type no longer needs to be specified

--- cogs/music.py
+++ cogs/music.py
@@ -70,11 +70,11 @@ class Music(commands.Cog):
     @commands.command()
     async def playf(self, ctx, *, query):
         """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        song = (query+'.webm')
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song))
         ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
 
-        await ctx.send('Now playing: {}'.format(query))
+        await ctx.send('Now playing: {}'.format(song))
 
     @commands.command()
     async def yt(self, ctx, *, url):
